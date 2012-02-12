c12baaaa5194af5489fc1d14ef8dbfa8bbff43e4
Nick Antonakos <nick.the.balrog@gmail.com>
2012-02-12 00:35:24 -0500

    messing with some vim tab options

--- src/perl/0024.pl
+++ src/perl/0024.pl
@@ -1,18 +1,18 @@
 
 my @num = (0..9);
-my $permu = 1;;
+my $permu = 1;
 foreach (2..10) {
-		$permu *= $_;
+    $permu *= $_;
 }
 
 my $test = 999999;
 my $final;
 my $loc;
 while (length($final)<10) {
-		$permu = int($permu / (10 - length($final)));
-		$loc = int($test / $permu);
-		$final .= splice(@num,$loc,1);
-		$test = $test - ($loc*$permu);
+    $permu = int($permu / (10 - length($final)));
+    $loc = int($test / $permu);
+    $final .= splice(@num,$loc,1);
+    $test = $test - ($loc*$permu);
 }
 
 print "$final\n";
