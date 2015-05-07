063f4833198468a929420d16d79e3e10846c079e
Burnz <benjamindiazf@openmailbox.org>
2015-05-07 14:04:41 -0300

    Reordered code

--- src/php/0003.php
+++ src/php/0003.php
@@ -1,13 +1,5 @@
 <?php 
 
-	function smallestFactor($n) {
-		for ($i = 2; $i <= $n ; $i++) { 
-			if ($n % $i == 0) {
-				return $i;
-			}
-		}
-	}
-
 	function largestPrimeFactor($n) {
 		$primeFactors = array();
 
@@ -15,6 +7,14 @@
 			$smallest = smallestFactor($n);
 			$primeFactors[] = $smallest;
 			return ($n == $smallest) ? max($primeFactors) : largestFactor($n / $smallest);
+		}
+
+		function smallestFactor($n) {
+			for ($i = 2; $i <= $n ; $i++) { 
+				if ($n % $i == 0) {
+					return $i;
+				}
+			}
 		}
 
 		return largestFactor($n);
